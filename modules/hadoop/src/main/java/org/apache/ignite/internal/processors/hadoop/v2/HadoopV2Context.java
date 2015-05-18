54c5794a607b2f7114133f0220ef8329e5ef8f10
evans_ye <innovansye@gmail.com>
2015-05-18 22:48:49 +0800

    take Ivan's suggestion

--- modules/hadoop/src/main/java/org/apache/ignite/internal/processors/hadoop/v2/HadoopV2Context.java
+++ modules/hadoop/src/main/java/org/apache/ignite/internal/processors/hadoop/v2/HadoopV2Context.java
@@ -90,7 +90,11 @@ public class HadoopV2Context extends JobContextImpl implements MapContext, Reduc
                 inputSplit = new FileSplit(new Path(fileBlock.file()), fileBlock.start(), fileBlock.length(), null);
             }
             else if (split instanceof HadoopExternalSplit)
-                throw new UnsupportedOperationException(); // TODO
+                try {
+                    inputSplit = (InputSplit) ((HadoopV2TaskContext)ctx).getNativeSplit(split);
+                } catch (IgniteCheckedException e) {
+                    e.printStackTrace();
+                }
             else if (split instanceof HadoopSplitWrapper)
                 inputSplit = (InputSplit) HadoopUtils.unwrapSplit((HadoopSplitWrapper) split);
             else
